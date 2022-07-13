b821f64cd575a228e2ce83c0c47ac1a20f871f11
Emilio Gozo <egozo@observatory.ph>
2022-07-13 10:41:19 +0800

    chore: use global envs

--- run_validation.sh
+++ run_validation.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #export FCST_YYYYMMDD="20200310"; export FCST_ZZ="00"; . $HOME/forecast/set_cron_env.sh; . $SCRIPT_DIR/set_date_vars.sh; . /home/modelman/forecast/validation/run_validation.sh
-PYTHONCONDA=/home/miniconda3/envs/toolbox/bin/python
-CDO=/opt/tools/nc/cdo
+
 export VAL_DIR=$MAINDIR/validation
 #export VAL_OUTDIR=$VAL_DIR/output/${FCST_YY}${FCST_MM}${FCST_DD}/$FCST_ZZ
 export VAL_OUTDIR=${OUTDIR}/validation/${FCST_YY}${FCST_MM}${FCST_DD}/$FCST_ZZ
@@ -12,7 +11,7 @@ export STATION_NAME="Manila Observatory"
 export CITY_ID="NCR"
 
 DATE_STR1=$(date -d "${FCST_YY}-${FCST_MM}-${FCST_DD} $FCST_ZZ:00:00 8 hours" +'%Y-%m-%d_%H')
-read FCST_YY_PHT FCST_MM_PHT FCST_DD_PHT FCST_HH_PHT <<<${DATE_STR1//[-_]/ }
+read -r FCST_YY_PHT FCST_MM_PHT FCST_DD_PHT FCST_HH_PHT <<<"${DATE_STR1//[-_]/ }"
 # -------------------------------------------- #
 #                 GSMaP                        #
 # -------------------------------------------- #
@@ -21,7 +20,7 @@ echo "--------------------------"
 echo " Processing GSMaP files  "
 echo "--------------------------"
 
-cd ${VAL_DIR}/gsmap
+cd "$VAL_DIR/gsmap" || exit
 
 rm ${VAL_DIR}/gsmap/dat/gsmap_nrt.${FCST_YY}${FCST_MM}${FCST_DD}*.dat
 # Download GSMaP data
@@ -31,8 +30,8 @@ rm ${VAL_DIR}/gsmap/dat/gsmap_nrt.${FCST_YY}${FCST_MM}${FCST_DD}*.dat
 ./convert_gsmap_nc.sh
 
 # Plot GSMaP
-mkdir -p $VAL_OUTDIR
-cd ${VAL_DIR}/grads
+mkdir -p "$VAL_OUTDIR"
+cd "$VAL_DIR/grads" || exit
 
 # Edit GrADS plotting script
 
@@ -55,18 +54,18 @@ echo "--------------------------"
 echo " Processing GFS files...  "
 echo "--------------------------"
 
-cd ${VAL_DIR}/gfs
+cd "$VAL_DIR/gfs" || exit
 
 # Convert GFS precipitation grb files to .nc
 ./convert_gfs_nc.sh
 
 # Plot GFS
-mkdir -p $VAL_OUTDIR
-cd ${VAL_DIR}/grads
+mkdir -p "$VAL_OUTDIR"
+cd "$VAL_DIR/grads" || exit
 
 # Edit GrADS plotting script
 DATE_STR1=$(date -d "${FCST_YY}-${FCST_MM}-${FCST_DD} $FCST_ZZ:00:00 8 hours" +'%Y-%m-%d_%H')
-read FCST_YY_PHT FCST_MM_PHT FCST_DD_PHT FCST_HH_PHT <<<${DATE_STR1//[-_]/ }
+read -r FCST_YY_PHT FCST_MM_PHT FCST_DD_PHT FCST_HH_PHT <<<"${DATE_STR1//[-_]/ }"
 DATE_STR2=$(date -d "${FCST_YY}-${FCST_MM}-${FCST_DD} $FCST_ZZ:00:00 32 hours" +'%Y-%m-%d_%H')
 DATE_STR3=$(date -d "${FCST_YY}-${FCST_MM}-${FCST_DD} $FCST_ZZ:00:00 56 hours" +'%Y-%m-%d_%H')
 DATE_STR4=$(date -d "${FCST_YY}-${FCST_MM}-${FCST_DD} $FCST_ZZ:00:00 80 hours" +'%Y-%m-%d_%H')
@@ -107,14 +106,14 @@ echo "--------------------------"
 # -------------------------------------------- #
 #            COMPARISON PLOTS                  #
 # -------------------------------------------- #
-9cho "---------------------------------"
+echo "---------------------------------"
 echo " Processing comparison plots...  "
 echo "---------------------------------"
 
 # 24-hr difference plot (WRF-GSMaP, GFS-GSMaP)
-cd ${VAL_DIR}/grads
+cd "$VAL_DIR/grads" || exit
 
-ln -s ${WRF_ENS}/wrffcst_d01_${FCST_YY}-${FCST_MM}-${FCST_DD}_${FCST_ZZ}_ens.nc ${VAL_DIR}/grads/nc/.
+ln -s "${WRF_ENS}/wrffcst_d01_${FCST_YY}-${FCST_MM}-${FCST_DD}_${FCST_ZZ}_ens.nc" ${VAL_DIR}/grads/nc/.
 
 # Edit GrADS plotting script
 sed -i "5s/.*/date='${DATE_STR1}PHT'/" bias_24hr_rain.gs
@@ -126,13 +125,13 @@ sed -i "9s~.*~wrfdir='nc'~" bias_24hr_rain.gs
 grads -pbc bias_24hr_rain.gs
 
 # 24-hr GSMaP versus TRMM Climatology
-mkdir -p ${OUTDIR}/climatology/${FCST_YY}${FCST_MM}${FCST_DD}/$FCST_ZZ
+mkdir -p "${OUTDIR}/climatology/${FCST_YY}${FCST_MM}${FCST_DD}/$FCST_ZZ"
 
-cd ${SCRIPT_DIR}/grads
+cd "$SCRIPT_DIR/grads" || exit
 
 # Edit GrADS plotting script
 DATE_STRM=$(date -d "${FCST_YY}-${FCST_MM}-${FCST_DD} $FCST_ZZ:00:00 8 hours" +'%Y-%B-%d_%H')
-read FCST_YY_PHT2 FCST_MM_PHT2 FCST_DD_PHT2 FCST_HH_PHT2 <<<${DATE_STR1//[-_]/ }
+read -r FCST_YY_PHT2 FCST_MM_PHT2 FCST_DD_PHT2 FCST_HH_PHT2 <<<"${DATE_STR1//[-_]/ }"
 DATE_STR2=$(date -d "${FCST_YY}-${FCST_MM}-${FCST_DD} $FCST_ZZ:00:00 32 hours" +'%Y-%m-%d_%H')
 
 sed -i "1s/.*/date='${DATE_STR1}PHT'/" gsmap_clim.gs
@@ -147,7 +146,7 @@ sed -i "11s~.*~monname='${FCST_MM_PHT2}'~" gsmap_clim.gs
 grads -pbc gsmap_clim.gs
 
 # 24-hr timeseries plot (WRF, GFS, GSMaP)
-cd ${VAL_DIR}/python
+cd "$VAL_DIR/python" || exit
 
 # Edit concatenating csv script
 sed -i "4s/.*/yyyymmdd = '${FCST_YY_PHT}-${FCST_MM_PHT}-${FCST_DD_PHT}'/" concat_rain.py
@@ -156,7 +155,7 @@ sed -i "6s~.*~IN_DIR = '${VAL_OUTDIR}'~" concat_rain.py
 sed -i "7s~.*~OUT_DIR = '${VAL_OUTDIR}'~" concat_rain.py
 
 # Concatenate csv
-$PYTHONCONDA concat_rain.py
+$PYTHON concat_rain.py
 
 # Edit time series plotting script
 sed -i "7s/.*/yyyymmdd = '${FCST_YY_PHT}-${FCST_MM_PHT}-${FCST_DD_PHT}'/" timeseries_plot.py
@@ -171,18 +170,15 @@ sed -i "15s/.*/station = '${STATION_NAME}'/" timeseries_plot.py
 sed -i "16s/.*/city_stn = '${CITY_ID}'/" timeseries_plot.py
 
 # Plot 24hr time series
-export MPLBACKEND="agg"
-$PYTHONCONDA timeseries_plot.py
+$PYTHON timeseries_plot.py
 
 # Edit time series plotting script
 
 # Plot 5-day time series WRF, AWS, and GSMaP
-export MPLBACKEND="agg"
-$PYTHONCONDA timeseries_plot_stations.py
+$PYTHON timeseries_plot_stations.py
 
 # Plot statistical error metrics (month summary only, runs every 5th day of month)
-export MPLBACKEND="agg"
-$PYTHONCONDA scalar_meaures.py
+$PYTHON scalar_meaures.py
 
 echo "--------------------------"
 echo " Done with comparison!    "
@@ -194,10 +190,10 @@ echo "---------------------------------"
 echo " Processing contingency plot...  "
 echo "---------------------------------"
 
-cd ${VAL_DIR}/grads/nc
+cd "$VAL_DIR/grads/nc" || exit
 $CDO remapbil,wrf_24hr_rain_day1_${DATE_STR1}PHT.nc gsmap_24hr_rain_day1_${DATE_STR1}PHT.nc gsmap_24hr_rain_day1_${DATE_STR1}PHT_re.nc
 
-cd ${VAL_DIR}/contingency
+cd "$VAL_DIR/contingency" || exit
 
 # Edit contingency script
 sed -i "4s/.*/yyyymmdd = '${FCST_YY_PHT}-${FCST_MM_PHT}-${FCST_DD_PHT}'/" forecast_verification.py
@@ -206,12 +202,10 @@ sed -i "6s~.*~EXTRACT_DIR = '${VAL_DIR}/grads/nc'~" forecast_verification.py
 sed -i "7s~.*~OUT_DIR = '${VAL_OUTDIR}'~" forecast_verification.py
 
 # Plot contingency table
-export MPLBACKEND="agg"
-$PYTHONCONDA forecast_verification.py
+$PYTHON forecast_verification.py
 
 # only executes during Sundays
-export MPLBACKEND="agg"
-$PYTHONCONDA forecast_verification_week_and_month_average.py
+$PYTHON forecast_verification_week_and_month_average.py
 
 echo "---------------------------"
 echo " Done with contingency!    "
@@ -228,4 +222,4 @@ rm ${VAL_DIR}/gfs/nc/${FCST_YY}${FCST_MM}${FCST_DD}/${FCST_ZZ}/*.nc
 echo "----------------------"
 echo " Validation finished! "
 echo "----------------------"
-cd ${VALDIR}
+cd "$VAL_DIR" || exit
